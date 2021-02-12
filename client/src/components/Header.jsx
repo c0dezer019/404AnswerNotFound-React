396d3f48ae2b19c42df0f409f1c23e60c02dcf5d
Brian Blankenship <brian.ga.edu@gmail.com>
2021-02-11 22:04:12 -0600

    Adding a logo.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Image } from 'semantic-ui-react';
+import img from '../images/logo.png';
 
 // eslint-disable-next-line no-unused-vars
-const Header = ({ user }) => (
-  <div id="header-body">
-  </div>
-);
+const Header = ({ user, mode }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [logoStyle, setLogoStyle] = useState('/client/src/images/404anflogo3.png');
 
-Header.propTypes = {
-  user: PropTypes.shape.isRequired,
+  useEffect(() => {
+    if (mode === 'dark') setLogoStyle('../images/404anflogo3_dark.png');
+  });
+
+  useEffect(() => {
+    if (mode === 'light') setLogoStyle('../images/404anflogo3.png');
+    else setLogoStyle('../images/404anflogo3_dark.png');
+  }, [mode]);
+
+  return (
+    <div id="header-body">
+      <Image
+        src={ img }
+        alt="logo"
+        as="a"
+        href="https:/404anf.bblankenship.me"
+      />
+    </div>
+  );
 };
+
+Header.propTypes =
+  {
+    user: PropTypes.shape.isRequired,
+    mode: PropTypes.string,
+  };
+
+Header.defaultProps =
+  {
+    mode: 'light',
+  };
 
 export default Header;
