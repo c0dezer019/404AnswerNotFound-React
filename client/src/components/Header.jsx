5fa094e94c4fae6473e18440056390e5da57921c
Brian Blankenship <brian.ga.edu@gmail.com>
2021-02-11 23:55:45 -0600

    Header Art.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,31 +1,65 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Image } from 'semantic-ui-react';
-import img from '../images/logo.png';
+import { AppBar, Badge, Menu, MenuItem, Toolbar, IconButton, InputBase} from '@material-ui/core';
+import { AccountCircle, MenuIcon, MoreIcon, NotificationsIcon, SearchIcon } from '@material-ui/icons';
+import logo from '../images/logo.png';
+import logoDark from '../images/logo_dark.png';
 
 // eslint-disable-next-line no-unused-vars
 const Header = ({ user, mode }) => {
-  // eslint-disable-next-line no-unused-vars
-  const [logoStyle, setLogoStyle] = useState('/client/src/images/404anflogo3.png');
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [logoStyle, setLogoStyle] = useState(logo);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    if (mode === 'dark') setLogoStyle('../images/404anflogo3_dark.png');
+    if (mode === 'dark') setLogoStyle(logoDark);
   });
 
   useEffect(() => {
-    if (mode === 'light') setLogoStyle('../images/404anflogo3.png');
-    else setLogoStyle('../images/404anflogo3_dark.png');
+    if (mode === 'light') setLogoStyle(logo);
+    else setLogoStyle(logoDark);
   }, [mode]);
 
   return (
-    <div id="header-body">
-      <Image
-        src={ img }
-        alt="logo"
-        as="a"
-        href="https:/404anf.bblankenship.me"
-      />
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        {/* Add menuButton style */}
+        <IconButton
+          aria-label="open drawer"
+          color="inherit"
+          edge="start"
+        >
+          <MenuIcon />
+        </IconButton>
+        <img src={ logoStyle } alt="Logo" />
+        {/* Add search style */}
+        <div>
+          {/* Add searchIcon style */}
+          <div>
+            <SearchIcon />
+          </div>
+          {/* Add mobile styles */}
+          <div>
+            {/* Add root and input styles */}
+            <InputBase
+              inputProps={{ 'aria-label': 'search' }}
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        {/* classes.grow goes here */}
+        <div />
+        {/* Add sectionDesktop style */}
+        <div>
+          <IconButton aria-label={ `show ${ notifications.length } new notifications` } color="inherit">
+            <Badge badgeContent={ notifications.length } color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
