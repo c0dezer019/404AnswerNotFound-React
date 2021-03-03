7102606ed19189f455beb83ec9caab9f260ebf4c
c0dezer019 <c0dezer019@zohomail.com>
2021-03-02 22:34:08 -0500

    Another redo

--- backend/anf_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnfAuthConfig(AppConfig):
-    name = 'anf_auth'
--- backend/anf_crud/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnfCrudConfig(AppConfig):
-    name = 'anf_crud'
